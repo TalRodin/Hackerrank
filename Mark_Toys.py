78c5b0f4b642ca9c50fdf6a73f0d4d19c7e83cb5
alyonarodin <alyona.rodin@gmail.com>
2018-08-19 15:49:23 -0400

    added

---
+++ Mark_Toys.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    sum_=0
+    total=0
+    
+    list.sort(prices)
+    print(prices)
+    for i in range(len(prices)):
+        if sum_+prices[i]<=k:
+            sum_=sum_+prices[i]
+            total=total+1
+        else:
+            break
+    return(total)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
